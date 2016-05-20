5abc17ba9e368a001341566752a1df09d59d277c
ababber2019 <ababber2019@mka.org>
2016-05-20 10:28:42 -0400

    Update Meld.py

--- Meld.py
+++ Meld.py
@@ -2,3 +2,9 @@
 #Meld.py
 #May 14 2016
 #An implementation of the meld superClass
+class Meld:
+  def __init__(self, setOrRun, cards):
+    self._setOrRun = setOrRun
+    self._cards = cards
+    for card in cards:
+      if card == 
