bf7c01edd26b0bfc3f8d59591845f6fd6c248ee2
GouwarPower <jgouwar@gmail.com>
2016-05-27 12:11:31 -0400

    Update ginRummy.py

--- ginRummy.py
+++ ginRummy.py
@@ -5,19 +5,23 @@
 from Player import *
 from Deck import *
 from Meld import *
+global roundOn
+global inTurns
+global wantAnotherAction 
 def getCardFromPlayer(player, card):
 	'''
-	This method takes a string that the user input and then returns the card object to be used by the program
+	This method takes an int that the user input and then returns the card object to be used by the program
 	:param player: The Player performing the action(Player object)
-	:param card: A string given in this form:ValueSuit, each as a single letter or number 
+	:param card: A int given as the position in the player's hand (using ordinal numbers, not indexing) 
 	return: the specific card object from the player's hand
 	'''
-	if card[0].isdigit()
-def takeAction(player, action):
+	return player._hand.index(card - 1)
+def takeAction(player, action, deck, discard):
 	'''
 	This method takes the action a player wants to make a preforms it
 	:param player: The player object performing the action 
 	:param action: One of the actions defined in README.txt as a string 
+	:params deck&discard: will always be the deck and discard pile, just need to be passed to the function
 	return: None
 	'''
 	if action == "draw":
@@ -30,16 +34,37 @@ def takeAction(player, action):
 			print("Sorry, invalid location!")
 			return None
 	elif action == "make set":
-		setTuple = ()
+		setArray = []
 		numCards = int(input("How many cards in this set?(3 or 4): "))
 		if numCards > 2 and numCards < 5:
 			for i in range(numCards):
-				card = input("Card to add (use rules for naming): ")
-				setTuple.append(getCardFromPlayer(player, card)) #adds the card to the set tuple
+				card = int(input("Card to add (use rules for naming): "))
+				setArray.append(getCardFromPlayer(player, card)) #adds the card to the set tuple
 		else:
-			print("Sorry, invalid num of times")
+			print("Sorry, invalid number of cards")
 			return None
+	elif action == "make run":
+		runArray = []
+		numCards = int(input("How many cards in this run"))
+		if numCards > 0 and numCards <= 11:  #hand size parameters
+			for i in range(numCards):
+				card = int(input("Card to add (use rules for naming): "))
+				runArray.append(getCardFromPlayer(player, card))
+		else:
+			print("Sorry, invalid number of cards")
+			return None
+	elif action == "knock":
+		player.knock()
+		roundOn = False 
+		inTurns = False	
+		wantAnotherAction = False
+	else: #block that condescends them for giving a non-valid action
+		print("Fine, be that way")
+		return None
 def main():
+	global roundOn
+	global inTurns
+	global wantAnotherAction
 	print("Welcome to the Gin Rummy Extravaganza! Press Enter to continue")
 	input()
 	player1Name = input("What is player 1's name?: ")
@@ -57,22 +82,43 @@ def main():
 			for i in range(10):
 				Player1.drawCard(deck)
 				Player2.drawCard(deck)
-			if player1Turn:
-				input("%s, press enter to continue, don't let %s see your actions" % (Player1.getName(), Player2.getName()))
-				print("\n" * 100) #clearing console because python is silly
-				print("Your hand:\n %a" % Player1.getHand())
-				wantAnotherAction = True 
-				while wantAnotherAction: #controling loop for turn actions
-					action = input("What action would you like to take?: ").lower()
-					takeAction(Player1, action)
-			else:
-				input("%s, press enter to continue, don't let %s see your actions" % (Player2.getName(), Player1.getName()))
-				print("\n" * 100) #clearing console because python is silly
-				print("Your hand:\n %a" % Player2.getHand())
-				wantAnotherAction = True 
-				while wantAnotherAction:
-					action = input("What action would you like to take?: ").lower()
-					takeAction(Player2, action)
+			inTurns = True
+			while inTurns: #controlling loop for turns 
+				if player1Turn:
+					input("%s, press enter to continue, don't let %s see your actions" % (Player1.getName(), Player2.getName()))
+					print("\n" * 100) #clearing console because python is silly
+					print("Your hand:\n %a" % Player1.getHand())
+					try: 
+						print("Top card of discard pile: " + discard._pile[len(discard._pile)-1])
+					except IndexError: #pile is empty
+						print("Discard pile is empty")
+					wantAnotherAction = True 
+					while wantAnotherAction: #controling loop for turn actions
+						action = input("What action would you like to take?: ").lower()
+						takeAction(Player1, action, deck, discard)
+						print("Your hand now:\n %a" % Player1.getHand())
+						anotherAction = input("Would you like to preform another action?(Y or N)").lower()
+						if "y" not in anotherAction: #anything that isn't yes is a no
+							cardToDiscard = int(input("What card would you like to discard?"))
+							Player1.discardCard(cardToDiscard, discard)
+							wantAnotherAction = False
+						player1Turn = False
+				else:
+					input("%s, press enter to continue, don't let %s see your actions" % (Player2.getName(), Player1.getName()))
+					print("\n" * 100) #clearing console because python is silly
+					print("Your hand:\n %a" % Player2.getHand())
+					wantAnotherAction = True 
+					while wantAnotherAction:
+						action = input("What action would you like to take?: ").lower()
+						takeAction(Player2, action, deck, discard)
+						print("Your hand now:\n %a" % Player2.getHand())
+						anotherAction = input("Would you like to preform another action?(Y or N)").lower()
+						if "y" not in anotherAction: #anything that isn't yes is a no
+							cardToDiscard = int(input("What card would you like to discard?"))
+							Player1.discardCard(cardToDiscard, discard)
+							print(Player1._hand)
+							wantAnotherAction = False
+					player1Turn = True
 		if Player1.getPoints() >= 100 or Player2.getPoints() >= 100: #score cap for the game
 			gameOn = False 
 	if Player1.getPoints() > Player2.getPoints():
@@ -80,7 +126,3 @@ def main():
 	elif Player2.getPoints() < Player1.getPoints():
 		winner = Player2
 main()
-			 
-			
-	
-  
